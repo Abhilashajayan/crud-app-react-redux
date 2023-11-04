5bbdb12f9c91bca4940b22529c424c6b0339d3a3
abhilashajayan2001 <abhilashajayan2001@gmail.com>
2023-11-04 22:11:39 +0530

    the home page row done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Signin  from './pages/user/signin'
 import { Route, Routes } from "react-router-dom";
 import Signup from './pages/user/signup';
-
+import Home from './pages/user/home';
 
 const  App = () => {
   return (
@@ -10,6 +10,7 @@ const  App = () => {
     <Routes>
       <Route path='/login' element={< Signin />} />
       <Route path='/signup' element={<Signup />} />
+      <Route path='/home' element={<Home />} />
     </Routes>
     
     </>
---
+++ client/src/pages/user/home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+
+
+const home = () => {
+  return (
+    <>
+       <Navbar />
+
+
+    </>
+  )
+}
+
+export default home
